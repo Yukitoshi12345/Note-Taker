81f7ed0e44d263942bf9122d4a96558120eb3354
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-02-24 21:57:55 +1100

    fsUtils.js completed

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,28 +1,43 @@
+// Importing the file system module
 const fs = require('fs');
+// Importing the utility module
 const util = require('util');
 
 // Promise version of fs.readFile
-const readFromFile = util.promisify(fs.readFile);
+const readFromFile = util.promisify(fs.readFile); // Promisifying the fs.readFile function
 
+// Function to write content to a file
 const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>  // Writing content to the file in JSON format
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)  // Logging success or error message
   );
 
+// Function to read from a file, append content to it, and write back to the file
 const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
+  fs.readFile(file, 'utf8', (err, data) => { // Reading from the file
     if (err) {
-      console.error(err);
+      console.error(err); // Logging error if encountered
     } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
+      const parsedData = JSON.parse(data); // Parsing the existing data as JSON
+      parsedData.push(content); // Appending new content
+      writeToFile(file, parsedData); // Writing updated content back to the file
     }
   });
 };
 
-// TODO: add deleting a note
-// delete note is just a placeholder including inside the module.exports
-const deleteNote = readFromFile
-module.exports = { readFromFile, writeToFile, readAndAppend, deleteNote };
+// Function to read from a file, delete content by idToDelete, and write back to the file
+const readAndDelete = (idToDelete, file) => {
+  fs.readFile(file, 'utf8', (err, data) => { // Reading from the file
+    if (err) {
+        console.error(err); // Logging error if encountered
+    } else {
+        const parsedData = JSON.parse(data); // Parsing the existing data as JSON
+        parsedData.filter(note => note.id !== idToDelete); // Filtering out the content with the specified id
+        writeToFile(file, parsedData); // Writing updated content back to the file
+      }
+    });
+  };
+
+// Exporting functions for external use
+module.exports = { readFromFile, writeToFile, readAndAppend, readAndDelete };
 
