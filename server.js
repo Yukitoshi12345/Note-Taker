a03e8f488b5546e6f58c3d33e8024fe8f7282cad
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-06 01:01:10 +1100

    updated

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Import the Express.js framework
-const express = require('express');
+const express = require("express");
 // Import the path module for working with file paths
-const path = require('path');
+const path = require("path");
 // Import the custom cLog middleware from the middleware/clog.js file
-const { clog } = require('./middleware/clog.js');
+const { clog } = require("./middleware/clog.js");
 // Import routes from the routes/index.js file
-const api = require('./routes/index.js');
+const api = require("./routes/index.js");
 
 // Set the port for the application, using a value from the environment variable PORT if available, or defaulting to 3001
 const PORT = process.env.PORT || 3001;
@@ -22,22 +22,21 @@ app.use(express.json());
 // Parse incoming URL-encoded data
 app.use(express.urlencoded({ extended: true }));
 // Mount the API routes defined in routes/index.js under the path /api
-app.use('/api', api);
+app.use("/api", api);
 
 // Serve static files from the public directory
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 // GET Route for notes page
 // Define a GET route for the notes page, serving the notes.html file
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 // GET Route for home page
 // Define a catch-all GET route for the home page, serving the index.html file
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/index.html'))
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/pages/index.html"))
 );
 
 // Start the Express server and listen on the specified port
