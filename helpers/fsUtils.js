b4094bd6bdd17c4a25c1b55fa7d40c07f2a5db58
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-06 00:57:57 +1100

    updated helpers and middleware

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,20 +1,28 @@
 // Importing the file system module
-const fs = require('fs');
+const fs = require("fs");
 // Importing the utility module
-const util = require('util');
+const util = require("util");
 
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile); // Promisifying the fs.readFile function
 
 // Function to write content to a file
 const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>  // Writing content to the file in JSON format
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)  // Logging success or error message
+  fs.writeFile(
+    destination,
+    JSON.stringify(content, null, 4),
+    (
+      err // Writing content to the file in JSON format
+    ) =>
+      err
+        ? console.error(err)
+        : console.info(`\nData written to ${destination}`) // Logging success or error message
   );
 
 // Function to read from a file, append content to it, and write back to the file
 const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => { // Reading from the file
+  fs.readFile(file, "utf8", (err, data) => {
+    // Reading from the file
     if (err) {
       console.error(err); // Logging error if encountered
     } else {
@@ -27,17 +35,17 @@ const readAndAppend = (content, file) => {
 
 // Function to read from a file, delete content by idToDelete, and write back to the file
 const readAndDelete = (id, file) => {
-  fs.readFile(file, 'utf8', (err, data) => { // Reading from the file
+  fs.readFile(file, "utf8", (err, data) => {
+    // Reading from the file
     if (err) {
-        console.error(err); // Logging error if encountered
+      console.error(err); // Logging error if encountered
     } else {
-        let parsedData = JSON.parse(data); // Parsing the existing data as JSON
-        parsedData = parsedData.filter(note => note.id !== id); // Filtering out the content with the specified id
-        writeToFile(file, parsedData); // Writing updated content back to the file
-      }
-    });
-  };
+      let parsedData = JSON.parse(data); // Parsing the existing data as JSON
+      parsedData = parsedData.filter((note) => note.id !== id); // Filtering out the content with the specified id
+      writeToFile(file, parsedData); // Writing updated content back to the file
+    }
+  });
+};
 
 // Exporting functions for external use
 module.exports = { readFromFile, writeToFile, readAndAppend, readAndDelete };
-
--- helpers/uuid.js
+++ helpers/uuid.js
@@ -1,7 +1,7 @@
 // Immediately export a function that generates a string of random numbers and letters
 module.exports = () =>
   // Generate a random number between 0 (inclusive) and 1 (exclusive), add 1, then multiply by 0x10000 and floor the result
-  // 0 x 10000 (hexadecimal value equivalent to 65536 in decimal).   
+  // 0 x 10000 (hexadecimal value equivalent to 65536 in decimal).
   Math.floor((1 + Math.random()) * 0x10000)
     // Convert the number to a hexadecimal string, and remove the first character
     .toString(16)
